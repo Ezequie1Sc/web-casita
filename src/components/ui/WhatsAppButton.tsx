919bfd57cdc95ea0413503bc6ab95d18ad0a0393
Ezequiel <tuemail@example.com>
2026-03-23 21:17:38 -0600

    actualoi

--- src/components/ui/WhatsAppButton.tsx
+++ src/components/ui/WhatsAppButton.tsx
@@ -13,7 +13,7 @@ interface WhatsAppButtonProps {
 
 export const WhatsAppButton: React.FC<WhatsAppButtonProps> = ({
   message,
-  phoneNumber = '529961136244',
+  phoneNumber = '529961332194',
   variant = 'whatsapp',
   size = 'md',
   fullWidth = false,
